dccfb7ea375feec6acfc651b2561791a64ef4c50
ayrama <70928574+ayrama@users.noreply.github.com>
2020-09-08 10:23:47 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,21 +7,34 @@ public class Main {
 	    String line = "";
         try {
             BufferedReader reader = new BufferedReader(new FileReader(path));
+		/** first 2 lines are description before the table
+		that's why I've read them and have not stored in an array
+		*/
+            reader.readLine();  
             reader.readLine();
-            reader.readLine();
+		/**200 line and 5 columnt in the chart to be processed
+		*/
             int row = 200;
             int col = 5;
             int i = 0;
             String[][] chart = new String[row][col];
+	    	/**reading and storing in the 2d array line by line till the Null result
+		*/
             while((line = reader.readLine()) != null){
                 chart[i]= line.split(",");
                 i++;
             }
-
+		/**PrintWriter method for creating and storing processed data in the .txt file
+		*/
             PrintWriter pr = new PrintWriter("ProducedChart.txt");
             int number = 1;
+		/** have used conditiopnal operator to print artist names without first and last " sign
+		*/
             pr.println(number + " " +
                     (chart[0][2].charAt(0) == '"'? chart[0][2].substring(1, chart[0][2].length() - 1) : chart[0][2]));
+		/**nested for loop for searching repetitions of the current name in already 
+		printed/processed list
+		*/
             for(int j = 1; j < 200; j++){
                 boolean bool = true;
                 for(int k = j-1; k == 0; k--){
